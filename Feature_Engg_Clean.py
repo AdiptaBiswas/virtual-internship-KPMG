cae9d1a0c90a9ebe445c996b5da03bee9fe5adcc
Adipta Biswas <40853486+AdiptaBiswas@users.noreply.github.com>
2020-09-22 22:26:16 +0530

    Module 2

--- Feature_Engg_Clean.py
+++ Feature_Engg_Clean.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 from datetime import datetime
 from datetime import date
 
 
-# In[2]:
-
-
-WBK = 'C:\\Users\\ADIPTA BISWAS\\Downloads\\KPMG_Data.xlsx'
+WBK = 'KPMG_Data.xlsx'
 Book = pd.read_excel(WBK, sheet_name=None)
 print(Book.keys())
-
-
-# In[3]:
 
 
 Demo_DF = pd.read_excel(WBK, sheet_name="CustomerDemographic")
@@ -26,9 +17,6 @@ Addr_DF= pd.read_excel(WBK, sheet_name="CustomerAddress")
 Tranx_DF= pd.read_excel(WBK, sheet_name="Transactions")
 New_Cust_DF = pd.read_excel(WBK, sheet_name="NewCustomerList")
 Demo_DF.head(3)
-
-
-# In[4]:
 
 
 def col_name_format(*df):
@@ -41,20 +29,11 @@ def col_name_format(*df):
     return DF_ls
 
 
-# In[5]:
-
-
 Demo_DF, Addr_DF, Tranx_DF, New_Cust_DF = col_name_format(Demo_DF, Addr_DF, Tranx_DF, New_Cust_DF)
-
-
-# In[6]:
 
 
 Demo_DF = Demo_DF.merge(Addr_DF, how = "left", on="customer_id")
 Demo_DF.where(Demo_DF.notnull(), np.nan)
-
-
-# In[7]:
 
 
 def nan_pcent(df):
@@ -63,34 +42,19 @@ def nan_pcent(df):
         print("* {} has {}% nan values\n".format(col,((df[col].isna().sum())/points)*100))
 
 
-# In[8]:
-
-
 print("Demo_DF shape: {}".format(Demo_DF.shape))
 print()
 nan_pcent(Demo_DF)
 
 
-# In[9]:
-
-
 Demo_DF.drop(columns=["first_name", "last_name", "default", "deceased_indicator", "address", "country"], inplace=True)
 
 
-# In[10]:
-
-
-Demo_DF["gender"] = Demo_DF["gender"].apply(lambda x: "Female" if x=="F" else "Female" if x=="Femal" else "Male" if x=="M" else "NA")
-
-
-# In[11]:
+Demo_DF["gender"] = Demo_DF["gender"].apply(lambda x: "Female" if x=="F" else "Female" if x=="Femal" else "Male" 
+                                            if x=="M" else "NA")
 
 
 print(list(set(Demo_DF["gender"])))
-
-
-# In[12]:
-
 
 def age_calculation(dob):
     dob = pd.to_datetime(dob)
@@ -103,13 +67,7 @@ Demo_DF['Age'] = Demo_DF['Age'].apply(lambda x: int(x) if isinstance(x, float) e
 print(set(Demo_DF["Age"].to_list()))
 
 
-# In[13]:
-
-
 Demo_DF.drop(columns="DOB",inplace=True)
-
-
-# In[14]:
 
 
 print("Tranx_DF shape: {}".format(Tranx_DF.shape))
@@ -117,19 +75,11 @@ print()
 nan_pcent(Tranx_DF)
 
 
-# In[15]:
-
-
-Tranx_DF.drop(columns=["transaction_date", "online_order", "order_status", "product_id", "transaction_id", "list_price", "standard_cost", "product_first_sold_date"], inplace=True)
-
-
-# In[16]:
+Tranx_DF.drop(columns=["transaction_date", "online_order", "order_status", "product_id", "transaction_id", 
+                       "list_price", "standard_cost", "product_first_sold_date"], inplace=True)
 
 
 Tranx_DF.head()
-
-
-# In[17]:
 
 
 def prod_class(*trnx_df):
@@ -141,13 +91,7 @@ def prod_class(*trnx_df):
 prod_class("brand", "product_line", "product_class", "product_size")
 
 
-# In[18]:
-
-
 Demo_DF_old = Demo_DF
-
-
-# In[19]:
 
 
 New_Cust_DF['Age'] = New_Cust_DF['DOB'].apply(age_calculation)
@@ -156,54 +100,26 @@ New_Cust_DF['Age'] = New_Cust_DF['Age'].apply(lambda x: int(x) if isinstance(x, 
 print(set(New_Cust_DF["Age"].to_list()))
 
 
-# In[20]:
-
-
 print([col for col in New_Cust_DF.columns.to_list()])
+
 print()
 New_Cust_DF = New_Cust_DF.loc[:, New_Cust_DF.columns.notnull()]
-
-
-# In[21]:
 
 
 New_Cust_DF["customer_id"] = [Demo_DF.shape[0]+1+i for i in range(len(New_Cust_DF))]
 
 
-# In[22]:
-
-
 New_Cust_DF = New_Cust_DF[[col for col in Demo_DF.columns.to_list()]]
-
-
-# In[23]:
 
 
 New_Cust_DF = Demo_DF.append(New_Cust_DF, ignore_index = True)
 
 
-# In[24]:
-
-
 New_Cust_DF
-
-
-# In[ ]:
 
 
 Demo_DF_old = Demo_DF_old.merge(Tranx_DF, how="inner", on="customer_id")
 
-
-# In[26]:
-
-
 Demo_DF_old.head()
 
-
 # *By:*  `Adipta Biwas`
-
-# In[ ]:
-
-
-
-
